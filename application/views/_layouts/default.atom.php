0e7c09dc899e280b580de2f0177b4191b1ee4f9e
Chris Wells <chriswells0@users.noreply.github.com>
2016-03-25 17:12:09 -0400

    Stop referencing Item and Items from view data.

--- application/views/_layouts/default.atom.php
+++ application/views/_layouts/default.atom.php
@@ -6,15 +6,7 @@
 	<id><?= 'tag:' . DOMAIN . ',2015:' . $ControllerURL . ':feed/atom' ?></id>
 	<link rel="self" type="<?= \CWA\Net\HTTP\HttpResponse::getContentType() ?>" href="<?= $ControllerURL ?>?format=atom" />
 	<link rel="alternate" type="text/html" href="<?= $ControllerURL ?>" />
-	<updated><?php
-$lastUpdated = null;
-$field = $Items[0]->getUpdatedFieldName();
-foreach ($Items as $item) { // Use the timestamp of the most recently updated item. -- cwells
-	if ($item->$field > $lastUpdated) $lastUpdated = $item->$field;
-}
-$lastUpdated = new DateTime($lastUpdated);
-echo $lastUpdated->format(DateTime::ATOM);
-?></updated>
+	<updated><?= $LastUpdated->format(DateTime::ATOM) ?></updated>
 	<title><?= SITE_NAME ?></title>
 	<subtitle><?= SITE_SLOGAN ?></subtitle>
 	<author>
