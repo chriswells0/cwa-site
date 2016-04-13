7d548f5a44c25f0146252af59893ae76bef42405
Chris Wells <chriswells0@users.noreply.github.com>
2016-04-12 23:14:24 -0400

    Change to PreviousPage and NextPage variables for pagination.

--- application/views/_shared/pagination.php
+++ application/views/_shared/pagination.php
@@ -1,11 +1,11 @@
 <?php
-if (!empty($BackURL) || !empty($NextURL)) {
+if (!empty($PreviousPage) || !empty($NextPage)) {
 	echo '<div id="pagination">';
-	if (!empty($BackURL)) {
-		echo '<a class="page-action" rel="prev" href="' . $BackURL . '" title="View previous page">← Previous Page</a>';
+	if (!empty($PreviousPage)) {
+		echo '<a class="page-action" rel="prev" href="' . $PreviousPage . '" title="View previous page">← Previous Page</a>';
 	}
-	if (!empty($NextURL)) {
-		echo '<a class="page-action" rel="next" href="' . $NextURL . '" title="View next page">Next Page →</a>';
+	if (!empty($NextPage)) {
+		echo '<a class="page-action" rel="next" href="' . $NextPage . '" title="View next page">Next Page →</a>';
 	}
 	echo "</div>\n";
 }
