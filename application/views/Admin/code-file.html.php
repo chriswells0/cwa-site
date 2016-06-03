f96be1472f5f3b2e4eaa40091671d243afa535cf
Chris Wells <chriswells0@users.noreply.github.com>
2016-06-03 08:57:42 -0400

    Move error message outside form.

--- application/views/Admin/code-file.html.php
+++ application/views/Admin/code-file.html.php
@@ -4,13 +4,13 @@ require_once 'views/_shared/status.html.php';
 				<div class="content">
 					<h1>Code Editor</h1>
 					<div class="content-body">
-						<form action="<?= $ControllerURL ?>/code" method="post">
 <?php if ($ReadOnly) { ?>
-							<h2>Viewing: <?= $FilePath ?></h2>
-							<p class="error">This file is not writable by the current user.</p>
+						<h2>Viewing: <?= $FilePath ?></h2>
+						<p class="error">This file is not writable by the current user.</p>
 <?php } else { ?>
-							<h2>Editing: <?= $FilePath ?></h2>
+						<h2>Editing: <?= $FilePath ?></h2>
 <?php } ?>
+						<form action="<?= $ControllerURL ?>/code" method="post">
 							<input type="hidden" name="file-path" value="<?= $FilePath ?>" />
 							<textarea id="file-contents" name="file-contents" wrap="off" <?= $ReadOnly ? 'readonly="true"' : '' ?> placeholder="This file is empty." autofocus required><?= $FileContents ?></textarea>
 <?php
