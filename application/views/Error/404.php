abdf45752d7b1a6139c41d956c8e9a768edbdebf
Chris Wells <chriswells0@users.noreply.github.com>
2016-09-11 18:39:51 -0400

    Reduce exclamation points: a 404 isn't that exciting.

--- application/views/Error/404.php
+++ application/views/Error/404.php
@@ -1,2 +1,2 @@
 	<h1>Page Not Found</h1>
-	<p>Oops! The page you were looking for was not found. Don't give up, though! It's probably around here somewhere.</p>
+	<p>Oops! The page you were looking for wasn't found. Don't give up, though: it's probably around here somewhere.</p>
